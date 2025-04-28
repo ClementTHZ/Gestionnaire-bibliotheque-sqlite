cb99757ee6bac99f11a9abcea368405115825188
Clément THOREZ <cthorez@selectour.fr>
2025-04-28 17:21:47 +0200

    Création des menus console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("========== MENU PRINCIPALE ==========");
+Console.WriteLine("1- Gérer les livres");
+Console.WriteLine("2- Gérer les lecteurs");
+Console.WriteLine("3- Emprunter un livre"); // Demander en console ID user et ID livre à emprunter
+Console.WriteLine("4- Rendre un livre"); // Demander en console ID emprunt
+Console.WriteLine("5- Afficher les emprunts");
+Console.WriteLine("6- Quitter");
+Console.WriteLine("=====================================");
+Console.WriteLine("> Votre choix :");
+
+Console.WriteLine("========== Gestion des livres ==========");
+Console.WriteLine("1- Ajouter un livre");
+Console.WriteLine("2- Supprimer un livre");
+Console.WriteLine("3- Lister les livres");
+Console.WriteLine("4- Lister les livres disponibles");
+Console.WriteLine("5- Retour au menu principal");
+Console.WriteLine("=====================================");
+Console.WriteLine("> Votre choix :");
+
+Console.WriteLine("========== Gestion des utilisateurs ==========");
+Console.WriteLine("1- Ajouter un utilisateur");
+Console.WriteLine("2- Supprimer un utilisateur");
+Console.WriteLine("3- Lister les utilisateurs");
+Console.WriteLine("4- Retour au menu principal");
+Console.WriteLine("=====================================");
+Console.WriteLine("> Votre choix :");
